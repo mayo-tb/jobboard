5f6df50f0668f3b6745dfef56b825345a125f232
mayo_tb <oyekanboluwatife6@gmail.com>
2025-11-06 23:43:01 +0100

    chagning url

--- jobboard_api/urls.py
+++ jobboard_api/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path, include
-from .api import *
-
 from rest_framework import routers
-
+from .api import *
 router = routers.DefaultRouter()
-
-router.register('router', bookviewset, basename = "router")
+router.register('router', bookviewset, basename="router")
 
 urlpatterns = [
     path("", include(router.urls)),
+
+    # user routes
+    path("users/", getuserdets),
+    path("users/add/", inputuserdata),
+    path("users/<int:id>/delete/", deleteuserdata),
+    path("users/<int:id>/update/", updateuserdata),
+
+    # application routes
+    path("applications/", getapplication),
+    path("applications/add/", inputapplication),
+    path("applications/<int:id>/delete/", delete_application),
+    path("applications/<int:id>/update/", updateapplication),
 ]
